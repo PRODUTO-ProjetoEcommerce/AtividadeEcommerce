d535dd61d1dd5a943f1164a27aa3a208a219e222
Debora <ds336756@gmail.com>
2025-04-11 14:14:06 -0300

    Criando o metodo Cadastrar no ProdutoRepositorio

--- ProjetoEcommerce/Models/Produto.cs
+++ ProjetoEcommerce/Models/Produto.cs
@@ -2,5 +2,10 @@
 {
     public class Produto
     {
+       public int CodProd { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int? Quantidade { get; set; }
+        public decimal? Preco { get; set; }
     }
 }
---
+++ ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoEcommerce.Models;
+using System.Data;
+
+namespace ProjetoEcommerce.Repositorio
+{
+    public class ProdutoRepositorio(IConfiguration configuration)
+    {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public void Cadastrar(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = MySqlCommand("Insert into Produto (Nome, Descricao, Quantidade, Preco) values (@nome, @descricao, @quantidade, @preco)", conexao);
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
+                cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+                cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+    }
+}
