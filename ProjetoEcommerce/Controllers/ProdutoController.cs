31481995476e9696cc95a8310aeb28b6494f809f
Debora <ds336756@gmail.com>
2025-04-11 13:56:46 -0300

    Criando o cadastrarProduto na controller

---
+++ ProjetoEcommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce.Models;
+using ProjetoEcommerce.Repositorio;
+
+namespace ProjetoEcommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produto produto)
+        {
+            _produtoRepositorio.CadastrarProduto(produto);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
