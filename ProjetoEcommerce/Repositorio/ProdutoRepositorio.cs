83301bae3227a7f1d4b1b08cefca1c0d108b1b45
Debora <ds336756@gmail.com>
2025-04-11 14:27:27 -0300

    Adicionando o metodo para editar no ProdutoRepositorio

--- ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
+++ ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
@@ -24,5 +24,31 @@ namespace ProjetoEcommerce.Repositorio
                 conexao.Close();
             }
         }
+
+        public bool Atualizar(Produto produto)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand("Update Produto set Nome = @nome, Descricao = @descricao, Quantidade = @quantidade, Preco = @preco where CodProd = @codigo", conexao);
+                    cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = produto.CodProd;
+                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
+                    cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
+                    cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+                    cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
+
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0;
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro ao atualizar produto: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
